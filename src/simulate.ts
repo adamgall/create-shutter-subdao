e47dc605fc9d8785096f4bf3d7a0cc5914eadb1d
Adam Gall <galladamj@gmail.com>
2024-04-17 15:54:39 -0400

    Working: predicting new module deployments through ModuleProxyFactory

--- src/simulate.ts
+++ src/simulate.ts
@@ -1,15 +1,25 @@
 import {
   Address,
+  Hex,
   PublicClient,
   bytesToBigInt,
+  encodeAbiParameters,
   encodeFunctionData,
   encodePacked,
   getContract,
   getContractAddress,
   hexToBigInt,
   keccak256,
+  parseAbiParameters,
+  toHex,
+  zeroAddress,
 } from "viem";
-import { GnosisSafeL2Abi, GnosisSafeProxyFactoryAbi } from "./abis";
+import {
+  FractalModuleAbi,
+  GnosisSafeL2Abi,
+  GnosisSafeProxyFactoryAbi,
+  ModuleProxyFactoryAbi,
+} from "./abis";
 import { randomBytes } from "crypto";
 
 export const simulate = async (publicClient: PublicClient) => {
@@ -19,6 +29,10 @@ export const simulate = async (publicClient: PublicClient) => {
     "0xc22834581ebc8527d974f8a1c97e1bea4ef910bc";
   const gnosisSafeL2SingletonAddress: Address =
     "0xfb1bffc9d739b8d520daf37df666da4c687191ea";
+  const moduleProxyFactoryAddress: Address =
+    "0xe93e4b198097c4cb3a6de594c90031cdac0b88f3";
+  const fractalModuleMasterCopyAddress: Address =
+    "0x1b26345a4a41d9f588e1b161b6e8f21d27547184";
 
   const gnosisSafeProxyFactory = getContract({
     address: gnosisSafeProxyFactoryAddress,
@@ -26,7 +40,13 @@ export const simulate = async (publicClient: PublicClient) => {
     client: publicClient,
   });
 
-  const initializer = encodeFunctionData({
+  const moduleProxyFactory = getContract({
+    address: moduleProxyFactoryAddress,
+    abi: ModuleProxyFactoryAbi,
+    client: publicClient,
+  });
+
+  const gnosisSafeInitializer = encodeFunctionData({
     abi: GnosisSafeL2Abi,
     functionName: "setup",
     args: [
@@ -45,9 +65,10 @@ export const simulate = async (publicClient: PublicClient) => {
   });
 
   const saltNonce = bytesToBigInt(randomBytes(32));
-  const salt = keccak256(
-    encodePacked(["bytes", "uint256"], [keccak256(initializer), saltNonce])
-  );
+  const salt = (initializer: Hex) =>
+    keccak256(
+      encodePacked(["bytes", "uint256"], [keccak256(initializer), saltNonce])
+    );
 
   const deploymentData = encodePacked(
     ["bytes", "uint256"],
@@ -61,16 +82,59 @@ export const simulate = async (publicClient: PublicClient) => {
     bytecode: deploymentData,
     from: gnosisSafeProxyFactoryAddress,
     opcode: "CREATE2",
-    salt,
+    salt: salt(gnosisSafeInitializer),
   });
+  console.log("predicted new safe address:", predictedSafeAddress);
 
   const { result: simulatedSafeAddress } =
     await gnosisSafeProxyFactory.simulate.createProxyWithNonce([
       gnosisSafeL2SingletonAddress,
-      initializer,
+      gnosisSafeInitializer,
       saltNonce,
     ]);
-
-  console.log("predicted new safe address:", predictedSafeAddress);
   console.log("simulated new safe address:", simulatedSafeAddress);
+
+  console.log("");
+
+  const fractalModuleInitializer = encodeFunctionData({
+    abi: FractalModuleAbi,
+    functionName: "setUp",
+    args: [
+      encodeAbiParameters(
+        parseAbiParameters("address, address, address, address[]"),
+        [
+          "0x0FAB989675bcbE3adFBAE4eC8E54061CcCE85020", // _owner // original parent safe // TODO update this
+          predictedSafeAddress, // _avatar // new child safe
+          predictedSafeAddress, // _target // new child safe
+          [], // _controllers
+        ]
+      ),
+    ],
+  });
+
+  const predictedFractalModuleAddress = getContractAddress({
+    bytecode: `0x602d8060093d393df3363d3d373d3d3d363d73${fractalModuleMasterCopyAddress.slice(
+      2
+    )}5af43d82803e903d91602b57fd5bf3`,
+    from: moduleProxyFactoryAddress,
+    opcode: "CREATE2",
+    salt: salt(fractalModuleInitializer),
+  });
+  console.log(
+    "predicted new fractal module address:",
+    predictedFractalModuleAddress
+  );
+
+  const { result: simulatedFractalModuleAddress } =
+    await moduleProxyFactory.simulate.deployModule([
+      fractalModuleMasterCopyAddress,
+      fractalModuleInitializer,
+      saltNonce,
+    ]);
+  console.log(
+    "simulated new fractal module address:",
+    simulatedFractalModuleAddress
+  );
+
+  console.log("");
 };
